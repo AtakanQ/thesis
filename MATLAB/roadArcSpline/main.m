6094fbf416f82e8c66713fcbc1bf38002a31ffdb
AtakanQ <47897767+AtakanQ@users.noreply.github.com>
2023-10-12 23:51:24 +0300

    Add files via upload

---
+++ MATLAB/roadArcSpline/main.m
@@ -0,0 +1,42 @@
+close all
+clear
+%% Define points
+
+pts = [5 5; 25 30; 55 55; 110 100;120 115; 125 130; 135 160];
+
+plot(pts(:,1), pts(:,2))
+hold on
+plot(pts(:,1), pts(:,2),'*')
+
+for i = 2: (length(pts) - 1)
+    %Calculate AB
+    AB = pts(i - 1,:) - pts(i,:);
+    AB = [AB 0];
+    normAB = norm( AB );
+    
+    %Calculate AB
+    AC = pts(i + 1,:) - pts(i,:); 
+    AC = [AC 0]; 
+    normAC = norm( AC );
+    
+    %Calculate angle
+    small_angle = atan2(norm(cross(AB,AC)),dot(AB,AC));
+
+    %Calculate D
+    norm_D = normAB * normAC * sin(small_angle);
+    D = [0 0 norm_D];
+
+    %Calculate D
+    E = cross(D,AB);
+    norm_E = norm(E);
+
+    %Calculate F
+    F = cross(D,AC);
+    norm_F = norm(F);
+
+    e = E / norm_E;
+    f = F / norm_F;
+
+    rho = ( normAC*normAC * E - normAB*normAB * F) / (2 * norm_D * norm_D);
+    1/norm(rho)
+end
