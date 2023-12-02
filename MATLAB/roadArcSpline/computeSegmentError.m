d4bd02fd32d5e4f8380004510c1f0de2de0c5b1d
AtakanQ <47897767+AtakanQ@users.noreply.github.com>
2023-12-02 19:38:17 +0300

    Added, computes error for single segment.

---
+++ MATLAB/roadArcSpline/computeSegmentError.m
@@ -0,0 +1,25 @@
+function [rms_error, max_error] = computeSegmentError(measurement_xy,ground_truth_xy)
+
+num_coor_arc = length(measurement_xy);
+errors = zeros(num_coor_arc,1);
+for k = 1:num_coor_arc
+    curr_point = measurement_xy(k,:);
+    
+    % Calculate Euclidean distances between 'curr_point' and all points in 'curr_clothoid_points'
+    distances = sqrt(sum(bsxfun(@minus, ground_truth_xy, curr_point).^2, 2));
+    
+    % Find the minimum of the closest point
+    [minError, ~] = min(distances);
+    errors(k) = minError;
+    % DEBUG
+    % plot(curr_point(1),curr_point(2),'*')
+    % hold on
+    % plot(curr_clothoid_points(:,1),curr_clothoid_points(:,2),'.','MarkerSize',15)
+end
+rms_error = rms(errors);
+max_error = max(errors);
+
+
+
+end
+
