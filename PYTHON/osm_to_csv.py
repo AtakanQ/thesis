ca52309c08c71a6951f3a0fa8b20d6b90ff4cf91
AtakanQ <47897767+AtakanQ@users.noreply.github.com>
2023-12-16 10:44:05 +0300

    extract germany constant turn road

--- PYTHON/osm_to_csv.py
+++ PYTHON/osm_to_csv.py
@@ -3,7 +3,8 @@ import csv
 
 #features = ox.features.features_from_bbox(lat1, lat2, lon1, lon2, {'highway': True})
 #features = ox.features.features_from_bbox(37.6636, 37.6218, 34.6771, 34.7412, {'highway': True}) #basmakci
-features = ox.features.features_from_bbox(52.357817, 52.310913, 11.996033,12.095047, {'highway': True}) #Hohenseeden - Parchen
+#features = ox.features.features_from_bbox(52.357817, 52.310913, 11.996033,12.095047, {'highway': True}) #Hohenseeden - Parchen
+features = ox.features.features_from_bbox(51.9941, 51.9848, 13.0050,13.0281, {'highway': True}) #Hohenseeden - Parchen
 road_tuples = [ ]
 road_counter = 0
 for i in range(len(features['geometry'])):
