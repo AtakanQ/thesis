4771c9481f9116f1bc32672d5c58feb216dc069c
AtakanQ <47897767+AtakanQ@users.noreply.github.com>
2024-03-12 21:48:25 +0300

    Revert "Add bezier.m"
    This reverts commit 06f1220492ef8975adb71e5a317d1499fe5d927b.

--- MATLAB/roadArcSpline/bezier.m
+++ MATLAB/roadArcSpline/bezier.m
@@ -26,6 +26,13 @@ classdef bezier < handle
     P_two
     P_three
     P_four
+    isInLaneArray
+    ltw
+    lra
+    lrb
+    vehicleBoundaries
+    Tangents
+    Curvatures
     end
     
     methods
@@ -39,7 +46,7 @@ classdef bezier < handle
             obj.final_tan = atan2(tfinal(2),tfinal(1));
             obj.curv_zero = curvature_zero;
             obj.curv_final = curvature_final;
-            obj.Nt = 10;
+            obj.Nt = 5;
             obj.Nk = 3;
             obj.mtmin = 0.3;
             obj.mtmax = 1.7;
@@ -47,6 +54,13 @@ classdef bezier < handle
             obj.mkmax = 5;
             num_curves  = obj.Nt * obj.Nt * obj.Nk * obj.Nk;
             obj.Curves = cell(num_curves, 1);
+            obj.Tangents = cell(num_curves, 1);
+            obj.Curvatures = cell(num_curves, 1);
+            obj.vehicleBoundaries.rearLeft = cell(num_curves, 1);
+            obj.vehicleBoundaries.rearRight = cell(num_curves, 1);
+            obj.vehicleBoundaries.frontLeft = cell(num_curves, 1);
+            obj.vehicleBoundaries.frontRight = cell(num_curves, 1);
+            obj.isInLaneArray = zeros(length(obj.Curves),1);
             obj.P_one = zeros(num_curves,2);
             obj.P_two = zeros(num_curves,2);
             obj.P_three = zeros(num_curves,2);
@@ -106,6 +120,7 @@ classdef bezier < handle
         end
         
         function generateCurves(obj,A,B)
+            t = (0:0.001:1)';
             P_zero = A;
             P_five = B;
             curve_position = 1;
@@ -120,16 +135,31 @@ classdef bezier < handle
                             obj.P_four(curve_position,:) = P_five - obj.tfinal_vectors(tan_final,:)/5;
             
                             obj.P_three(curve_position,:) = obj.acceleration_final(acc_final,:)/20 + 2*obj.P_four(curve_position,:) - P_five;
-                            obj.Curves{curve_position} = zeros(101,2);
-                            pos = 1;
-                            for t = 0:0.01:1
-                                 obj.Curves{curve_position}(pos,:) =...
-                                     (1-t)^5 *P_zero + 5*t*(1-t)^4*obj.P_one(curve_position,:) +...
-                                      10* t^2 *(1-t)^3 *obj.P_two(curve_position,:) + 10 * t^3*(1-t)^2 *obj.P_three(curve_position,:)+...
-                                     +5*t^4*(1-t)*obj.P_four(curve_position,:) + t^5*P_five;
-                                 pos = pos +1;
-                            end
-            
+                            obj.Curves{curve_position} = zeros(1001,2);
+                            obj.Tangents{curve_position} = zeros(1001,1);
+                            obj.Curvatures{curve_position} = zeros(1001,1);
+                            % pos = 1;
+
+                            obj.Curves{curve_position} =...
+                                 (1-t).^5 *P_zero + 5*t.*(1-t).^4*obj.P_one(curve_position,:) +...
+                                  10* t.^2 .*(1-t).^3 *obj.P_two(curve_position,:) + 10 * t.^3.*(1-t).^2 *obj.P_three(curve_position,:)+...
+                                 +5*t.^4.*(1-t)*obj.P_four(curve_position,:) + t.^5*P_five;
+
+                            % dX = 5 * t.^4 * (obj.P_one(curve_position,:) - P_zero) + ...
+                            %         20 * t.^3 * (obj.P_two(curve_position,:) - 2 * obj.P_one(curve_position,:) + P_zero) + ...
+                            %         30 * t.^2 * (obj.P_three(curve_position,:) - 3 * obj.P_two(curve_position,:) + 3 * obj.P_one(curve_position,:) - P_zero) + ...
+                            %         20 * t * (obj.P_four(curve_position,:) - 4 * obj.P_three(curve_position,:) + 6 * obj.P_two(curve_position,:) - 4 * obj.P_one(curve_position,:) + P_zero) + ...
+                            %          5 * ones(length(t),1) * (P_five - obj.P_four(curve_position,:) + obj.P_three(curve_position,:) - obj.P_two(curve_position,:) + obj.P_one(curve_position,:) - P_zero);
+                            dx = diff(obj.Curves{curve_position}(:, 1));
+                            dy = diff(obj.Curves{curve_position}(:, 2));
+                            obj.Tangents{curve_position} = [atan2(dy,dx); obj.final_tan];
+                            
+                                % Compute differential arc lengths
+                            ds = sqrt(dx.^2 + dy.^2);
+                            
+                            % Compute curvatures
+                            obj.Curvatures{curve_position} =[obj.curv_zero; (diff(obj.Tangents{curve_position}(1:(end-1)) ) ./ ds(2:end)) ; obj.curv_final];
+
                             curve_position = curve_position+1;
                         end
                     end
@@ -138,6 +168,48 @@ classdef bezier < handle
 
         end
         
+        function addVehicleDimensions(obj,ltw ,lra, lrb)
+            %The vehicle's position is assumed to be the center of rear
+            %axle. dimensions = [ltw lla llb]
+            ninety = pi/2 * ones(length(obj.Tangents{1}),1);
+            obj.ltw = ltw;
+            obj.lra = lra;
+            obj.lrb = lrb;
+            for j = 1:length(obj.Curves)
+                last_vector = [cos(obj.Tangents{j}(end)) sin(obj.Tangents{j}(end))];
+                last_point = obj.Curves{j}(end,:);
+
+                to_left_angle = ninety + obj.Tangents{j};
+                to_left_vectors = [cos(to_left_angle) sin(to_left_angle)] * obj.ltw / 2;
+                to_front_vectors =[cos(obj.Tangents{j}) sin(obj.Tangents{j})] * obj.lra;
+
+                obj.vehicleBoundaries.rearLeft{j} = obj.Curves{j} + to_left_vectors;
+                obj.vehicleBoundaries.rearRight{j} = obj.Curves{j} - to_left_vectors;
+                obj.vehicleBoundaries.frontLeft{j} = obj.vehicleBoundaries.rearLeft{j} + to_front_vectors;
+                obj.vehicleBoundaries.frontRight{j} = obj.vehicleBoundaries.rearRight{j} + to_front_vectors;
+
+                %Remove exceeding points
+                allVectors = obj.vehicleBoundaries.frontRight{j} - repmat(last_point,length(obj.vehicleBoundaries.frontRight{j}),1);
+                angles = abs( atan2(allVectors(:,2),allVectors(:,1)) - atan2(last_vector(2),last_vector(1)) );
+                angles(angles>pi) = 2*pi - angles(angles>pi);
+                indices = angles<(pi/2);
+                obj.vehicleBoundaries.frontLeft{j}(indices,:) = obj.vehicleBoundaries.rearLeft{j}(indices,:);
+                obj.vehicleBoundaries.frontRight{j}(indices,:) = obj.vehicleBoundaries.rearRight{j}(indices,:);
+            end
+            figure;
+            
+            plot(obj.Curves{1}(:,1),obj.Curves{1}(:,2),'DisplayName','Curve')
+            axis equal
+            hold on
+            plot(obj.vehicleBoundaries.rearLeft{1}(:,1),obj.vehicleBoundaries.rearLeft{1}(:,2),'DisplayName','RearLeft')
+            plot(obj.vehicleBoundaries.rearRight{1}(:,1),obj.vehicleBoundaries.rearRight{1}(:,2),'DisplayName','RearRight')
+            plot(obj.vehicleBoundaries.frontLeft{1}(:,1),obj.vehicleBoundaries.frontLeft{1}(:,2),'DisplayName','FrontLeft') 
+            plot(obj.vehicleBoundaries.frontRight{1}(:,1),obj.vehicleBoundaries.frontRight{1}(:,2),'DisplayName','FrontRight') 
+            title('Example')
+            legend();
+        end
+        
+
         function plotCurves(obj)
 
             figure;
@@ -155,10 +227,10 @@ classdef bezier < handle
                     color = floor(i/100) / 9;
                     plot(obj.Curves{i}(:,1),obj.Curves{i}(:,2),'Color',[0 color color])
                     hold on
-                    plot(obj.P_one(i,1),obj.P_one(i,2),'o')
-                    plot(obj.P_two(i,1),obj.P_two(i,2),'o')
-                    plot(obj.P_three(i,1),obj.P_three(i,2),'o')
-                    plot(obj.P_four(i,1),obj.P_four(i,2),'o')
+                    % plot(obj.P_one(i,1),obj.P_one(i,2),'o')
+                    % plot(obj.P_two(i,1),obj.P_two(i,2),'o')
+                    % plot(obj.P_three(i,1),obj.P_three(i,2),'o')
+                    % plot(obj.P_four(i,1),obj.P_four(i,2),'o')
                     disp('Debugging bezier')
                 end
 
@@ -167,6 +239,28 @@ classdef bezier < handle
             annotation('textbox', [0.2, 0.1, 0.1, 0.1], 'String', string( ['init tan:',num2str(rad2deg(obj.init_tan)),...
                 '  init curv:',num2str(obj.curv_zero),'  final tan:',num2str(rad2deg(obj.final_tan)),'  final curv:',num2str(obj.curv_final) ] ),'FitBoxToText','on')
 
+        end
+
+        function isInLane(obj,segment)
+            % segments data points should not be sparse. It should be as
+            % frequent as possible
+            sz = size(segment.allX);
+            
+            for i = 1:length(obj.Curves)
+                if sz(1) == 1
+                    [rms_error, max_error, errors] = computeSegmentError(obj.Curves{i},[segment.allX' segment.allY']);
+                elseif sz(2) == 1
+                    [rms_error, max_error, errors] = computeSegmentError(obj.Curves{i},[segment.allX segment.allY]);
+                else
+                    disp('There is some sort of error with dimensions')
+                end
+
+                if(sum(errors > 1.8) > 0) %There is some curve that is far from lane center
+                    obj.isInLaneArray(i) = 0;
+                else
+                    obj.isInLaneArray(i) = 1;
+                end
+            end
         end
     end
 end
