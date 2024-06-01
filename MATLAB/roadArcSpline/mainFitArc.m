f0c1ffaf9f47e7324a35d4b5fc663ed23f755894
AtakanQ <47897767+AtakanQ@users.noreply.github.com>
2024-06-01 21:45:26 +0300

    Bezier plotting part added.

--- MATLAB/roadArcSpline/mainFitArc.m
+++ MATLAB/roadArcSpline/mainFitArc.m
@@ -1,14 +1,49 @@
 clear
 
-
-%% Try to fit clothoid
-% load('bezierData.mat')
 load("all_clothoids.mat")
 xError =  +0.2; %meters in x direction
 yError = -0.01; %mteres is y direction
 headingError = deg2rad(5); %radians
 curvatureError = -0.01;
-
 clothoid = all_clothoids{1}(158);
-[clothoidArray] = fitArcSpline([clothoid.allX(1)+xError clothoid.allY(1)+yError],...
-    clothoid.init_tan+headingError,clothoid.init_curv + curvatureError,clothoid);
+
+% Ego vehicle attitude
+vehicleX = clothoid.allX(1)+xError;
+vehicleY = clothoid.allY(1)+yError;
+vehicleTan = clothoid.init_tan+headingError;
+vehicleCurv = clothoid.init_curv + curvatureError;
+%% Try to fit clothoid
+
+[clothoidArray,middleArc] = fitArcSpline([vehicleX vehicleY],...
+    vehicleTan,vehicleCurv,clothoid);
+
+figure;
+plot(clothoid.allX,clothoid.allY,'Color',[0 1 0],'DisplayName','Ground Truth')
+hold on
+axis equal
+for i = 1:numel(clothoidArray)
+    clothoidArray(i).plotPlain([i/7 0 0], "Clothoid num:" + num2str(i));
+end
+plot(middleArc.x_coor,middleArc.y_coor,"DisplayName","Arc","LineWidth",2)
+legend();
+%% Try to fit a Bézier curve
+
+[trajectories] = fitBezier([vehicleX vehicleY],...
+    vehicleTan,vehicleCurv,clothoid);
+
+trajectories.plotCurves(5);
+axis equal
+hold on
+plot(clothoid.allX,clothoid.allY,'Color',[0 1 0],'DisplayName','Ground Truth')
+legend();
+
+%% Plot both
+trajectories.plotCurves(5);
+axis equal
+hold on
+plot(clothoid.allX,clothoid.allY,'Color',[0 1 0],'DisplayName','Ground Truth')
+for i = 1:numel(clothoidArray)
+    clothoidArray(i).plotPlain([i/7 0 0], "Clothoid num:" + num2str(i));
+end
+plot(middleArc.x_coor,middleArc.y_coor,"DisplayName","Arc","LineWidth",2)
+legend();
