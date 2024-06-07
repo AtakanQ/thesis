c28dd252b8b5a8741892179d7422b2bb72c62e45
AtakanQ <47897767+AtakanQ@users.noreply.github.com>
2024-06-07 12:13:28 +0300

    variation of control points added.

--- MATLAB/plotGenerate/plotBezier.m
+++ MATLAB/plotGenerate/plotBezier.m
@@ -3,7 +3,7 @@ addpath('../roadArcSpline/')
 A = [0 0];
 B = [15 10];
 tzero = [1 0]';
-tfinal = [sqrt(2) sqrt(2)]';
+tfinal = [sqrt(2) -sqrt(2)]';
 init_curvature = 0;
 final_curvature = .01;
 
@@ -11,8 +11,26 @@ final_curvature = .01;
 trajectories = bezier(A,B,tzero,tfinal,init_curvature,final_curvature);
 
 trajectories.plotCurves(1);
-title("Bézier Curve Example","FontSize",13)
+title("Bézier Curve Example","FontSize",1, 3)
 xlabel("xEast (m)","FontSize",13)
 ylabel("yNorth (m)","FontSize",13)
 
+%% Plot varying P1
 
+trajectories.plotCurvesByControlPoint(1);
+legend();
+ 
+%% Plot varying P2
+
+trajectories.plotCurvesByControlPoint(2);
+legend();
+
+%% Plot varying P3
+
+trajectories.plotCurvesByControlPoint(3);
+legend();
+
+%% Plot varying P4
+
+trajectories.plotCurvesByControlPoint(4);
+legend();
